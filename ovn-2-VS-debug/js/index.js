d5f1165333b501ed4cec3d4a98c4c66d9bc47bb4
zocom-kim-vagi <54845585+zocom-kim-vagi@users.noreply.github.com>
2019-10-11 22:15:26 +0200

    Debugga med VS code

---
+++ ovn-2-VS-debug/js/index.js
@@ -0,0 +1,24 @@
+const getFirstNumber = () => {
+    let number = document.querySelectorAll('input')[0].value;
+    return number;
+}
+const getSecondNumber = () => {
+    let number = document.querySelectorAll('input')[1].value;
+    return number;
+};
+
+const sum = () => {
+    let firstNumber = getFirstNumber();
+    let secondNumber = getSecondNumbe();
+    let sum = firstNumber + secondNumber;
+    return sum;
+};
+
+const showSum = () => {
+    let elem = document.querySelectorAll('p')[2];
+    elem.innerHTML = getFirstNumber() + ' + ' + getSecondNumber() + ' = ' + sum();
+};
+
+document.querySelector('button').addEventListener('click', () => {
+    showSum();
+});
